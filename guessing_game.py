85a0a3fec94670234f7122076c5282206ae6cac4
rdelva <roseldelva@gmail.com>
2024-09-24 17:13:06 -0400

    commented score method

--- guessing_game.py
+++ guessing_game.py
@@ -12,49 +12,71 @@ def generate_number():
         return random.randint(1,10)
 
 
+# def score(current_score =""):
+#      #Keeps track of the current score (lowest score) 
+          
+#     #  if highest_score == "":
+#     #       return "No Score"  
+#     #  elif current_score < highest_score:
+#     #       highest_score = current_score # make the current score the highest_score 
+#     #       return highest_score
+#     #  elif current_score > highest_score:
+#     #       return highest_score   # send back the original highest score   
+#     #  elif current_score == highest_score:
+#     #         return highest_score
+
+
 
 # Create the start_game function.
 def start_game():
-#   When the program starts, we want to:
-#   ------------------------------------
-#   1. Display an intro/welcome message to the player.
-#   2. Store a random number as the answer/solution.
-#   3. Continuously prompt the player for a guess.
-#     a. If the guess is greater than the solution, display to the player "It's lower".
-#     b. If the guess is less than the solution, display to the player "It's higher".
+    #   When the program starts, we want to:
+    #   ------------------------------------
+    #   1. Display an intro/welcome message to the player.
+    #   2. Store a random number as the answer/solution.
+    #   3. Continuously prompt the player for a guess.
+    #     a. If the guess is greater than the solution, display to the player "It's lower".
+    #     b. If the guess is less than the solution, display to the player "It's higher".
 
-#   4. Once the guess is correct, stop looping, inform the user they "Got it"
-#      and show how many attempts it took them to get the correct number.
-#   5. Let the player know the game is ending, or something that indicates the game is over.
+    #   4. Once the guess is correct, stop looping, inform the user they "Got it"
+    #      and show how many attempts it took them to get the correct number.
+    #   5. Let the player know the game is ending, or something that indicates the game is over.
 
-# ( You can add more features/enhancements if you'd like to. )
-    #Display intro/welcome message
-   
-    name = input("What is your name? ")
-    print("Hello {},  and welcome to The Number Guessing Game. I'm your host Rose!! \n Today you will guess a number between 1 to 10."
-          .format(name))
+    # ( You can add more features/enhancements if you'd like to. )
+        #Display intro/welcome message
+
+
+    scores = []
+    print("Hello, and welcome to The Number Guessing Game. I'm your host Rose!! \n Today you will guess a number between 1 to 10.")
     answer = input("Are you ready to play? (yes/no) " )
-    
-    score = [] #create a scorebox to keep a record of all the scores
+ 
 
-    while answer.lower() == 'yes': 
+    while answer.lower() == 'yes':               
+        
+        name = input("What is your name? ")
+      
 
+        if len(scores) == 0:
+             print("Hi {}, There is no high score today!".format(name))
+        else:
+            print("Hi {}, Today's highest score is {} ".format(name , min(scores)))    
+
+        
+        
         counter = 0       
         max_number = 10
         min_number = 1   
         random_number = generate_number()  # generates a random number and assigns it to random_number      
-        game_over = False  # A flag that allows the guessing loop to continue 
+        game_over = False  # A flag that allowsDid I misunderstand  the guessing loop to continue                  
         
-        #Only prints out the highest score if the array is filled and selects the highest score
-        if len(score):
-            print("Highest score for the Guessing Game is: {}".format( min(score)))             
-
-
+        
+     
+        
         while game_over == False: #continue to run the game until the user guesses the random number
             
             try:
                 guess = input("Please guess a number between 1 to 10. ")           
                 guess = int(guess)
+                
               
                 #Raise a ValueError if the guess is higher than the max_number
                 if guess > max_number:
@@ -65,7 +87,7 @@ def start_game():
                 print("Please enter a whole number. {}".format(err))             
             else: 
                 #keep guessing until guess == random_number
-                
+                    
                         if guess < random_number:
                             print("Guess again. The number is higher")
                             counter += 1
@@ -75,16 +97,17 @@ def start_game():
                             counter += 1
                             game_over = False
                         else:
-                            print("That's correct. The number is {}. It tooks you {} tries. ".format(random_number, counter))                       
-                            score.append(counter)
+                            counter += 1
+                            scores.append(counter)
+                            print("That's correct. The number is {}. It tooks you {} tries. ".format(random_number, counter))                     
                             answer = input("Do you want to play again? ")
                             game_over = True
                             
 
     
-    print("Thank you for playing, {}! Good Bye :) ".format(name))
-           
-  
 
-# Kick off the program by calling the start_game function.
+           
+    print("Thank you for playing! Good Bye :) ")
+
+
 start_game()
